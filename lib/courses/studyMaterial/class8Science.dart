b4193acdb5208feca9ef6d7ee48e3c55d46c61ef
Rakshit Khandelwal <rakshitkhandelwal1234@gmail.com>
2020-11-05 20:04:30 +0530

    Rakshit 5th nov. 8:04

---
+++ lib/courses/studyMaterial/class8Science.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Science81 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science82 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science83 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science84 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science85 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science86 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science87 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science88 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science89 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science810 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science811 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science812 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science813 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science814 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Sciencne815 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Sciencne816 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Sciencne817 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Sciencne818 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
---
+++ lib/courses/studyMaterial/class9Science.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Science91 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science92 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science93 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science94 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science95 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science96 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science97 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science98 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science99 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science910 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science911 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science912 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science913 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science914 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
+
+class Science915 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:
+          ('https://drive.google.com/file/d/1v-kIyRFrmBtUHVrGqngUqaqTITSow3DK/view'),
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
