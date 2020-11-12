8cec09ae57b5d568cdbb8c97c5323a08e1bd2265
Rakshit Khandelwal <rakshitkhandelwal1234@gmail.com>
2020-11-12 17:59:25 +0530

    Rakshit science appbar

--- lib/courses/science.dart
+++ lib/courses/science.dart
@@ -37,7 +37,7 @@ class _ScienceState extends State<Science> {
             ),
             onPressed: () => Navigator.pop(context),
           ),
-          backgroundColor: Color(0xff03258C),
+          backgroundColor: Color.fromRGBO(0, 51, 100, 1),
           title: Text(
             'ERA',
             style: TextStyle(
